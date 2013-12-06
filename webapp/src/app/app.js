bb58a6ff0548abd609d9db39b3773611054f9f62
alessandro arnodo <a.arnodo@gmail.com>
2013-12-07 00:38:29 +0100

    documentation goes well

--- webapp/src/app/app.js
+++ webapp/src/app/app.js
@@ -1,44 +1,53 @@
 window.app = window.app || {};
 
 app.bootstrap = function() {
-    angular.bootstrap(document, ['app']);
+	angular.bootstrap(document, ['app']);
 };
 
 app.init = function() {
-    app.bootstrap();
+	app.bootstrap();
 };
 
 app.config = {
-    apiUrl: 'api/v1',
-    version: "0.0.1"
+	apiUrl: 'api/v1',
+	version: "0.0.1"
 };
 
 angular.element(document).ready(function() {
-    app.init();
+	app.init();
 });
 
 angular.module('app', [
-    'templates.app',
-    'templates.common',
-    'app.home',
-    'app.notes',
-    'app.docs',
-    'ngRoute',
-    'ngAnimate',
-    'chieffancypants.loadingBar',
-    'common.directives.appVersion',
-    'common.directives.plusOne',
-    'common.interceptors.http'
+	'templates.app',
+	'templates.common',
+	'app.home',
+	'app.notes',
+	'app.docs',
+	'ngRoute',
+	'ngAnimate',
+	'chieffancypants.loadingBar',
+	'common.directives.appVersion',
+	'common.directives.plusOne',
+	'common.interceptors.http'
 ]).config(function myAppConfig($provide, $routeProvider, $locationProvider, $httpProvider) {
-    $locationProvider.html5Mode(true);
-    $routeProvider.otherwise({
-        redirectTo: '/'
-    });
+	/*/
+	$provide.decorator('$sniffer', function($delegate) {
+		$delegate.history = false;
+		return $delegate;
+	});
+	*/
+	$locationProvider.html5Mode(true);
+	$routeProvider.otherwise({
+		redirectTo: '/home'
+	});
 }).run(function run($rootScope, $window) {
-    $rootScope.config = $window.app.config;
+	$rootScope.config = $window.app.config;
+	$rootScope.$on('$routeChangeStart', function(next, current) {
+		$window.scrollTo(0, 0);
+	});
 }).controller('AppCtrl', function AppCtrl($scope, $location) {
-    $scope.title = "ng-kickstart | easy AngularJS development";
-    $scope.isActive = function(viewLocation) {
-        return (viewLocation === $location.path());
-    };
+	$scope.title = "ng-kickstart | easy AngularJS development";
+	$scope.isActive = function(viewLocation) {
+		return (viewLocation === $location.path());
+	};
 });
--- webapp/src/app/home/home.js
+++ webapp/src/app/home/home.js
@@ -1,7 +1,7 @@
 angular.module('app.home', [
 	'ngRoute'
 ]).config(function config($routeProvider) {
-	$routeProvider.when('/', {
+	$routeProvider.when('/home', {
 		controller: 'HomeCtrl',
 		templateUrl: 'app/home/home.tpl.html'
 	});
