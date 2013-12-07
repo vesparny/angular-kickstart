77fc2bf4d8c1744f69c6b6918bdbd07bf614968b
alessandro arnodo <a.arnodo@gmail.com>
2013-12-07 16:03:31 +0100

    docs comments

--- build/build.config.js
+++ build/build.config.js
@@ -1,45 +1,60 @@
 'use strict';
 
 module.exports = {
-	buildDir: 'build/tmp',
-	distDir: 'build/dist',
-	buildFiles: ['build/**/*.js', '!build/tmp/**', '!build/dist/**', '!build/*.config.js'],
-	buildConf: ['build/*.config.js'],
-	banner: '/* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd")  %> */\n',
-	proxy: {
-		context: '/api',
-		host: 'localhost',
-		port: 9001
-	},
-	connect: {
-		port: 9000,
-		livereload: 35729,
-		hostname: 'localhost'
-	},
-	webappFiles: {
-		js: ['webapp/src/app/**/*.js', 'webapp/src/common/**/*.js'],
-		assets: ['assets/**'],
-		tests: ['webapp/test/**/*.spec.js'],
-		templatesApp: ['webapp/src/app/**/*.tpl.html'],
-		templatesCommon: ['webapp/src/common/**/*.tpl.html'],
-		sass: 'webapp/src/sass/main.scss',
-		sassFiles: ['webapp/src/sass/**/*.scss'],
-		html: 'webapp/index.tpl.html'
-	},
-	vendorFiles: {
-		js: [
-			'webapp/vendor/angular/angular.js',
-			'webapp/vendor/angular-route/angular-route.js',
-			'webapp/vendor/angular-resource/angular-resource.js',
-			'webapp/vendor/angular-animate/angular-animate.js',
-			'webapp/vendor/angular-loading-bar/build/loading-bar.js',
-			'webapp/vendor/alertify/alertify.js'
-		],
-		css: [
-			'webapp/vendor/foundation/css/normalize.css',
-			'webapp/vendor/angular-loading-bar/build/loading-bar.min.css',
-			'webapp/vendor/alertify/themes/alertify.core.css',
-			'webapp/vendor/alertify/themes/alertify.default.css'
-		]
-	}
+    buildDir: 'build/tmp',
+    distDir: 'build/dist',
+    buildFiles: ['build/**/*.js', '!build/tmp/**', '!build/dist/**', '!build/*.config.js'],
+    buildConf: ['build/*.config.js'],
+    /*
+     * banner to prepend on the generated files
+     */
+    banner: '/* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd")  %> */\n',
+    /*
+	 * your proxy configuration, change it as you want.
+	 */
+    proxy: {
+        context: '/api',
+        host: 'localhost',
+        port: 9001
+    },
+    /*
+	 * connect configurations used during development.
+	 */
+    connect: {
+        port: 9000,
+        livereload: 35729,
+        hostname: 'localhost'
+    },
+    /*
+     * paths of your app files, they will be automagically loaded and watched by the build.
+     */
+    webappFiles: {
+        js: ['webapp/src/app/**/*.js', 'webapp/src/common/**/*.js'],
+        assets: ['assets/**'],
+        tests: ['webapp/test/**/*.spec.js'],
+        templatesApp: ['webapp/src/app/**/*.tpl.html'],
+        templatesCommon: ['webapp/src/common/**/*.tpl.html'],
+        sass: 'webapp/src/sass/main.scss',
+        sassFiles: ['webapp/src/sass/**/*.scss'],
+        html: 'webapp/index.tpl.html'
+    },
+    /*
+     * bower files to load, they are not watched during the development, the order matters.
+     */
+    vendorFiles: {
+        js: [
+            'webapp/vendor/angular/angular.js',
+            'webapp/vendor/angular-route/angular-route.js',
+            'webapp/vendor/angular-resource/angular-resource.js',
+            'webapp/vendor/angular-animate/angular-animate.js',
+            'webapp/vendor/angular-loading-bar/build/loading-bar.js',
+            'webapp/vendor/alertify/alertify.js'
+        ],
+        css: [
+            'webapp/vendor/foundation/css/normalize.css',
+            'webapp/vendor/angular-loading-bar/build/loading-bar.min.css',
+            'webapp/vendor/alertify/themes/alertify.core.css',
+            'webapp/vendor/alertify/themes/alertify.default.css'
+        ]
+    }
 };
--- build/karma.config.unit.js
+++ build/karma.config.unit.js
@@ -1,69 +1,55 @@
 'use strict';
 
 module.exports = function(karma) {
-  karma.configure({
-    /**
-     * From where to look for files, starting with the location of this file.
+	karma.configure({
+		/*
+		 * From where to look for files, starting with the location of this file.
+		 */
+		basePath: '../',
+		/*
+		 * This is the list of file patterns to load into the browser during testing.
+		 */
+		files: [
+			'webapp/vendor/angular/angular.js',
+			'webapp/vendor/angular-mocks/angular-mocks.js',
+			'webapp/vendor/angular-route/angular-route.js',
+			'webapp/vendor/angular-resource/angular-resource.js',
+			'webapp/vendor/angular-animate/angular-animate.js',
+			'webapp/vendor/angular-loading-bar/build/loading-bar.js',
+			'webapp/vendor/alertify/alertify.js',
+			'webapp/src/**/*.js',
+			'build/tmp/templates*.js',
+			'webapp/test/**/*.spec.js'
+		],
+		exclude: [
+			'webapp/src/assets/**/*.js'
+		],
+		frameworks: ['jasmine'],
+		plugins: ['karma-jasmine', 'karma-firefox-launcher', 'karma-chrome-launcher', 'karma-phantomjs-launcher'],
+		/*
+		 * How to report, by default.
+		 */
+		reporters: 'dots',
+		/*
+		 * On which port should the browser connect, on which port is the test runner
+		 * operating, and what is the URL path for the browser to use.
+		 */
+		port: 9018,
+		runnerPort: 9100,
+		urlRoot: '/__test/',
+
+		/**
+		 * Disable file watching by default.
+		 */
+		autoWatch: false,
+
+		/*
+     which Browser to use
      */
-    basePath: '../',
-
-    /**
-     * This is the list of file patterns to load into the browser during testing.
+		browsers: ['Chrome'],
+		/*
+     if true, it capture browsers, run tests and exit
      */
-    files: [
-      'webapp/vendor/angular/angular.js',
-      'webapp/vendor/angular-mocks/angular-mocks.js',
-      'webapp/vendor/angular-route/angular-route.js',
-      'webapp/vendor/angular-resource/angular-resource.js',
-      'webapp/vendor/angular-animate/angular-animate.js',
-      'webapp/vendor/angular-loading-bar/build/loading-bar.js',
-      'webapp/vendor/alertify/alertify.js',
-      'webapp/src/**/*.js',
-      'build/tmp/templates*.js',
-      'webapp/test/**/*.spec.js'
-    ],
-    exclude: [
-      'webapp/src/assets/**/*.js'
-    ],
-    frameworks: ['jasmine'],
-    plugins: ['karma-jasmine', 'karma-firefox-launcher', 'karma-chrome-launcher', 'karma-phantomjs-launcher'],
-
-
-    /**
-     * How to report, by default.
-     */
-    reporters: 'dots',
-
-    /**
-     * On which port should the browser connect, on which port is the test runner
-     * operating, and what is the URL path for the browser to use.
-     */
-    port: 9018,
-    runnerPort: 9100,
-    urlRoot: '/__test/',
-
-    /**
-     * Disable file watching by default.
-     */
-    autoWatch: false,
-
-    /**
-     * The list of browsers to launch to test on. This includes only "Firefox" by
-     * default, but other browser names include:
-     * Chrome, ChromeCanary, Firefox, Opera, Safari, PhantomJS
-     *
-     * Note that you can also use the executable name of the browser, like "chromium"
-     * or "firefox", but that these vary based on your operating system.
-     *
-     * You may also leave this blank and manually navigate your browser to
-     * http://localhost:9018/ when you're running tests. The window/tab can be left
-     * open and the tests will automatically occur there during the build. This has
-     * the aesthetic advantage of not launching a browser every time you save.
-     */
-    browsers: ['Chrome'],
-
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: true
-  });
+		singleRun: true
+	});
 };
